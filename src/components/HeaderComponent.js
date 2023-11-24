4b7a75a30e8f38934ea6e3a4f3ee978629e273c2
manasa-kalluru <mns90852@gmail.com>
2023-11-24 00:00:20 -0500

    mockData

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,21 +1,21 @@
-import {SIDENAV_IMG} from "../utils/constants";
+import { SIDENAV_IMG } from "../utils/constants";
 import { LOGO_URL } from "../utils/constants";
 const HeaderComponent = () => {
-    return (
-      <div className="header">
-        <div className="logo-items">
+  return (
+    <div className="header">
+      <div className="logo-items">
         <img className="sideNav" src={SIDENAV_IMG} />
         <img className="logo" src={LOGO_URL} />
-        </div>
-        <div className="nav-items">
+      </div>
+      <div className="nav-items">
         <ul>
           <li>Location</li>
           <li>SearchBar</li>
           <li>Cart</li>
         </ul>
-        </div>
       </div>
-    );
-  };
+    </div>
+  );
+};
 
-  export default HeaderComponent;
+export default HeaderComponent;
