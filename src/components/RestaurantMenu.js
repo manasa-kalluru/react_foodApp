c6d275f874838a081102ae5742f568cea3d25da0
manasa-kalluru <mns90852@gmail.com>
2023-12-07 00:04:09 -0500

    custom hooks

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,28 +1,11 @@
-import { useState, useEffect } from "react";
 import Shimmer from "./Shimmer";
 import { useParams } from "react-router-dom";
-import { MENU_API } from "../utils/constants";
+import useRestaurantMenu from "../utils/useRestaurantMenu";
 
 const RestaurantMenu = () => {
-  const [resInfo, setResInfo] = useState(null);
+  const { resId } = useParams();
 
-  const {resId} = useParams();
-  
-
-  useEffect(() => {
-    fetchMenu();
-  }, []);
-
-  const fetchMenu = async () => {
-    const data = await fetch(MENU_API + resId);
-    const json = await data.json();
-    console.log(json);
-    setResInfo(json?.data);
-
-    // const uber = await fetch("https://corsproxy.io/?https://www.ubereats.com/_p/api/getFeedV1");
-    // const uberJson = await uber.json();
-    // console.log(uberJson);
-  };
+  const resInfo = useRestaurantMenu(resId);
 
   if (resInfo === null) {
     return <Shimmer />;
@@ -45,10 +28,10 @@ const RestaurantMenu = () => {
       {itemCards.map((card) => {
         const { name, id, price } = card?.card?.info;
         return (
-          <div className="menu-items" key = {id}>
+          <div className="menu-items" key={id}>
             <ul>
               <li className="itemName">
-                {name} - Rs.{price/100}
+                {name} - Rs.{price / 100}
               </li>
             </ul>
           </div>
